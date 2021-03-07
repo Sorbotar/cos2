85d07cada96c58a080362aafc9f2b7711d441a48
Dobromil Rykaluk <rykaluk15@gmail.com>
2021-03-07 16:04:39 +0100

    dodanie plikow

---
+++ src/zad/zadd.java
@@ -0,0 +1,13 @@
+package zad;
+
+class zadd {
+	 public static void main(String args[]){
+	     System.out.println("W pierwszym branchu; 2+3=");
+//	     System.out.println(policz(2,3));
+	 }
+	 
+//	 public static int policz(int a,int b) {
+//			return a+b;
+//	 }
+	 
+	}
